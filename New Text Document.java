62244ba4d0e225426e3639f2805f53f455d46fe7
sidharthpandita1 <150953942+sidharthpandita1@users.noreply.github.com>
2024-08-31 08:43:26 +0530

    Add files via upload

---
+++ New Text Document.java
@@ -0,0 +1,9 @@
+// Import the necessary classes
+import java.lang.System;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
